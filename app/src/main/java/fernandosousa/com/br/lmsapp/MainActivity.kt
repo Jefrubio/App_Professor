25736071b71eb17bbfbf6133be377b40ab394044
fsousa <fesousa>
2018-07-01 17:38:36 -0300

    Initial commit

---
+++ app/src/main/java/fernandosousa/com/br/lmsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package fernandosousa.com.br.lmsapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
