febcd49560a1db2dad935661419befb1df675a94
fsousa <fesousa>
2018-08-18 12:53:48 -0300

    Imagens Android

--- app/src/main/java/fernandosousa/com/br/lmsapp/DisciplinaService.kt
+++ app/src/main/java/fernandosousa/com/br/lmsapp/DisciplinaService.kt
@@ -7,8 +7,8 @@ object DisciplinaService {
     fun getDisciplinas (context: Context): List<Disciplina> {
         val disciplinas = mutableListOf<Disciplina>()
 
-        // criar 3 disciplinas
-        for (i in 1..3) {
+        // criar 10 disciplinas
+        for (i in 1..10) {
             val d = Disciplina()
             d.nome = "Disciplina $i"
             d.ementa = "Ementa Disciplina $i"
