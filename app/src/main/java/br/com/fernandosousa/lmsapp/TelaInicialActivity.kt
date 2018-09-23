486ba8ff1eafdc3cf5214a7477c4f0c45ead6e3f
fsousa <fesousa>
2018-09-23 19:50:09 -0300

    Atualizacao

--- app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/fernandosousa/lmsapp/TelaInicialActivity.kt
@@ -1,9 +1,8 @@
-package fernandosousa.com.br.lmsapp
+package br.com.fernandosousa.lmsapp
 
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
